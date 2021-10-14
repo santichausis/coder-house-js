f97ff39f9ad5e574f576667234fb8d2bbe863475
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2021-10-14 10:13:30 -0300

    Ejercicios primera clase

--- Clase-01/app.js
+++ Clase-01/app.js
@@ -1,2 +1,14 @@
-let nombre = prompt("Ingrese su nombre");
-console.log("Hola "+ nombre + " bienvenido al curso de Javascript!")
+// Ejercicio 1
+let pedirNombre = prompt("Ingrese su nombre");
+console.log("Hola "+ pedirNombre + " bienvenido al curso de Javascript!")
+
+// Ejercicio 2
+let pedirNumero = prompt("Ingrese un número");
+let numero1 = parseInt(pedirNumero)
+let numero2 = 20;
+console.log(numero1 + numero2);
+
+// Ejercicio 3
+let pedirHobbie = prompt("¿Qué te gusta hacer?")
+let pedirHobbie2 = prompt("Aparte de esa actividad ¿cuál otra te gusta?");
+alert("Te gusta " + pedirHobbie + " como así también " + pedirHobbie2);
