55e3094cef032ba4c14d71d268fb8385ca393fec
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2021-10-21 09:46:15 -0300

    Se agrega if else

--- Clase-02/app.js
+++ Clase-02/app.js
@@ -6,8 +6,10 @@ let total = parseInt(nota1) + parseInt(nota2) + parseInt(nota3);
 let promedio = total / 3;
 
 // Sacar promedio de notas
-if ( promedio >= 7) {
-    alert("¡Felicitaciones! Aprobaste la materia con un promedio de " + promedio)
+if ( promedio >= 8) {
+    alert("¡Felicitaciones! Aprobaste la materia con un promedio de " + promedio);
+} else if (promedio >=6){
+    alert("¡Felicitaciones! Aprobaste la cursada, ya podes rendir el examen final. Recordá que tu promedio fue de " + promedio);
 } else {
-    alert("A seguir estudiando. Tu promedio fue " + promedio + ". Se necesita 7 o más para aprobar la cursada.")
-};
+    alert("A seguir estudiando. Tu promedio fue " + promedio + ". Se necesita 7 o más para aprobar la cursada.");
+}
