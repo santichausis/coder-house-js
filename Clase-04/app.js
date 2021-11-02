f269038a1cd441102856440da27fe75e8a3aab77
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2021-11-02 09:15:17 -0300

    Se agrega Clase 4

---
+++ Clase-04/app.js
@@ -0,0 +1,46 @@
+// Calcular precio de la compra de plantas y tierra.
+
+let plantas = Number(prompt("¿Cuántas plantas quiere? El valor de cada una es de $135"))
+let tierra = Number(prompt("¿Cuántas bolsas de tierra quiere? El valor de cada bolsa es de $150"))
+let envio = prompt("¿Necesita que se lo envien a su domicilio? Si/No. El valor es $300")
+
+function precioPlantas(plantas) {
+    totalPlantas = plantas * 135
+    document.write(`🌿 En plantas usted gastó un total de $${totalPlantas}. <br>`)
+}
+
+function precioTierra(tierra) {
+    totalTierra = tierra * 150
+    document.write(`🪨 En tierra usted gastó un total de $${totalTierra}. <br>`)
+}
+
+function calcularEnvio(envio) {
+    if (envio == "Si") {
+        totalEnvio = 300;
+        document.write(`🚛 El precio del envio es de $300 <br>`)
+    } else {
+        totalEnvio = 0;
+        document.write(`🚛 El precio del envio es de $0 <br>`)
+    }
+}
+
+function gastoTotal(totalPlantas, totalTierra) {
+    precioFinal = totalPlantas + totalTierra + totalEnvio
+    document.write(`💰 En total usted gastó $${totalPlantas + totalTierra + totalEnvio}. <br>`)
+}
+
+function descuento(precioFinal) {
+    if (precioFinal > 1000) {
+        precioFinal = precioFinal * 0.9
+        document.write(`💸 En total con descuentos usted gastó ${precioFinal}.`)
+    } else {
+        document.write(`😕 No aplican descuentos en este monto.`)
+    }
+}
+
+precioPlantas(plantas)
+precioTierra(tierra)
+calcularEnvio(envio)
+gastoTotal(totalPlantas, totalTierra)
+descuento(precioFinal)
+
