c91d4a837b9507ee7c0b03c9e29d24d01cdd2318
Santiago Chausis <santiagochausis@iMac-de-Santi.local>
2021-11-10 18:14:40 -0300

    Pre entrega trabajo final

--- Proyecto-final/app.js
+++ Proyecto-final/app.js
@@ -1,106 +1,136 @@
-//  Calcular precio de la compra de plantas y tierra.
-let plantas = Number(
-  prompt('¿Cuántas plantas quiere? El valor de cada una es de $135')
-);
-let tierra = Number(
-  prompt('¿Cuántas bolsas de tierra quiere? El valor de cada bolsa es de $150')
-);
-let envio = prompt(
-  '¿Necesita que se lo envien a su domicilio? Si/No. El valor es $300'
-);
-
-// Calcular costo de las plantas
-function precioPlantas(plantas) {
-  totalPlantas = plantas * 135;
-  document.getElementById(
-    'plantas'
-  ).innerHTML = `🌿 En plantas usted gastó un total de $${totalPlantas} <br>`;
-}
-
-// Calcular costo de la tierra
-function precioTierra(tierra) {
-  totalTierra = tierra * 150;
-  document.getElementById(
-    'tierra'
-  ).innerHTML = `🪨 En tierra usted gastó un total de $${totalTierra} <br>`;
-}
-
-// Calcular envio
-function calcularEnvio(envio) {
-  if (envio.toLowerCase() == 'si') {
-    totalEnvio = 300;
-    document.getElementById(
-      'envio'
-    ).innerHTML = `🚛 El precio del envio es de $300 <br>`;
-  } else {
-    totalEnvio = 0;
-    document.getElementById(
-      'envio'
-    ).innerHTML = `🚛 El precio del envio es de $0 <br>`;
-  }
-}
-
-// Calcular gasto total
-function gastoTotal(totalPlantas, totalTierra) {
-  precioFinal = totalPlantas + totalTierra + totalEnvio;
-  document.getElementById('total').innerHTML = `💰 En total usted gastó $${
-    totalPlantas + totalTierra + totalEnvio
-  } <br>`;
-}
-
-// Calcular descuento
-function descuento(precioFinal) {
-  if (precioFinal > 1000) {
-    precioFinal = precioFinal * 0.9;
-    document.getElementById(
-      'totalDesc'
-    ).innerHTML = `💸 En total con descuentos usted gastó $${precioFinal}`;
-  } else {
-    document.getElementById(
-      'totalDesc'
-    ).innerHTML = `😕 No aplican descuentos en este monto. Total a pagar $${precioFinal}`;
-  }
-}
-
-// Llamar funciones
-precioPlantas(plantas);
-precioTierra(tierra);
-calcularEnvio(envio);
-gastoTotal(totalPlantas, totalTierra);
-descuento(precioFinal);
-
-//  Arrays
-const suculentas = [
-  { id: 01, entorno: 'interior', nombre: 'Sansevieria', precio: 120 },
-  { id: 02, entorno: 'exterior', nombre: 'Cactus de Navidad', precio: 120 },
-  { id: 03, entorno: 'exterior', nombre: 'Aloe vera', precio: 120 },
-  { id: 04, entorno: 'exterior', nombre: 'Rosa del desierto', precio: 500 },
-  { id: 05, entorno: 'interior', nombre: 'Echeveria', precio: 300 },
-];
-
-// Usar array
-for (const plantas of suculentas) {
-  console.log(plantas.id, plantas.nombre, plantas.precio);
-}
-
-const entorno = suculentas.filter((planta) => planta.entorno == 'exterior');
-console.log(entorno);
-
-// Class Suculenta
-
-class Suculentas {
-  constructor(id, entorno, nombre, precio) {
+// Crear clase Libro con todos los atributos que va a tener
+class Libro {
+  constructor(id, nombre, autor, año, editorial, portada, precio, stock) {
     this.id = id;
-    this.entorno = entorno;
     this.nombre = nombre;
+    this.autor = autor;
+    this.año = año;
+    this.editorial = editorial;
+    this.portada = portada;
     this.precio = precio;
-  }
-  mostrarSuculentas() {
-    console.log(Suculentas);
+    this.stock = stock;
   }
 }
 
-// Agregar Suculentas
-const Suculenta1 = new Suculentas(01, 'exterior', 'Sansevieria', 375);
-const Suculenta2 = new Suculentas(02, 'interior', 'Echeveria', 525);
-const Suculenta3 = new Suculentas(03, 'exterior', 'Aloe vera', 175);
+// Crear array vacio de libros para luego almacenarlos y manipularlos
+const libros = [];
+
+libros.push(
+  new Libro(
+    01,
+    'Todos somos villanos',
+    'M.L.RIO',
+    2017,
+    'Penguin',
+    'portada1.jpg',
+    1500,
+    25
+  )
+);
+libros.push(
+  new Libro(
+    02,
+    'Agujas doradas',
+    'Michael McDowell',
+    2021,
+    'La bestia equilatera',
+    'portada2.jpg',
+    2185,
+    95
+  )
+);
+libros.push(
+  new Libro(
+    03,
+    'A prueba de fuego',
+    'Javier Moro',
+    2020,
+    'Espasa',
+    'portada3.jpg',
+    1800,
+    55
+  )
+);
+libros.push(
+  new Libro(
+    04,
+    'Una educación',
+    'Tara Westover',
+    2018,
+    'Lumen',
+    'portada4.jpg',
+    1900,
+    15
+  )
+);
+libros.push(
+  new Libro(
+    05,
+    'La chica salvaje',
+    'Delia Owens',
+    2019,
+    'Vintage Espanol',
+    'portada5.jpg',
+    1340,
+    5
+  )
+);
+libros.push(
+  new Libro(
+    06,
+    'El instituto',
+    'Stephen King',
+    2019,
+    'Plaza & Janés',
+    'portada6.jpg',
+    1760,
+    20
+  )
+);
+
+// Almacenar de forma local
+localStorage.setItem('libros', JSON.stringify(libros));
+
+// Recorrer el array y mostarlo en el DOM
+for (const libro of libros) {
+  let div = document.createElement('div');
+  div.classList.add('cardLibro');
+  div.innerHTML = `
+                  <img class='cardImage' src='./assets/images/portadas/${libro.portada}'/>
+                  <h2 class='cardTitle'>${libro.nombre}</h2>
+                  <h3 class='cardAuthor'>${libro.autor}</h3>
+                  <p class='cardPublisher'>${libro.editorial}</p>
+                  <p class='cardPrice' id='cardPrice'>$${libro.precio}</p>
+                  `;
+
+  document.getElementById('arrayLibros').appendChild(div);
+
+  // Validar precio y aplicar descuento 
+  function aplicarDescuento() {
+    if (libro.precio > 2000) {
+      document
+        .getElementById('cardPrice')
+        .replaceWith(`¡20% OFF! $${libro.precio * 0.8}`);
+    } else if (libro.precio > 1500) {
+      document
+        .getElementById('cardPrice')
+        .replaceWith(`¡15% OFF! $${libro.precio * 0.85}`);
+    } else if (libro.precio > 1000) {
+      document
+      .getElementById('cardPrice')
+      .replaceWith(`¡10% OFF! $${libro.precio * 0.9}`);
+    }
+  }
+  aplicarDescuento();
+}
+
+// Ordenar array
+libros.sort(function (a, b) {
+  if (a.nombre > b.nombre) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.table(libros)
